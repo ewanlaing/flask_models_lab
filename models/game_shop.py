50483b027eac1d919ac79e35a0d46bd491662f66
Ewan Laing <ewanlaing@Ewans-MacBook-Air.local>
2023-06-01 09:11:44 +0100

    added some more css

--- models/game_shop.py
+++ models/game_shop.py
@@ -2,5 +2,6 @@ from models.order import *
 
 order1 = Order("Paul", "31.5.23", 1, "God of War")
 order2 = Order("Ewan", "30.11.2023", 2, "System Shock")
+order3 = Order("Ian", "1.6.23", 1, "Tomb Raider")
 
-orders=[order1, order2]
+orders=[order1, order2, order3]
